cbb7bcf4e1ec7c4106f3782de969de3f4fceb296
raisyam <rmfbkw04@gmail.com>
2023-02-15 19:20:10 +0700

    initial file

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'sass-loader',
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.join(__dirname, 'client', 'public', 'index.html'),
+		}),
+		// new CopyWebpackPlugin({
+		//	 patterns: [
+		//		 {
+		//			 from: path.resolve(__dirname, 'client', 'public', 'assets'),
+		//			 to: path.resolve(__dirname, 'dist'),
+		//		 },
+		//	 ],
+		// }),
+	],
+};
