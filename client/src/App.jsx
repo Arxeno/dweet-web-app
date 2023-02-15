01cd96a7e9aba7964728266dab448c513f1cdebb
raisyam <rmfbkw04@gmail.com>
2023-02-15 22:27:32 +0700

    add navbar template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
 	return (
 		<div className="App">
-			Hello World
+			<Navbar />
 		</div>
 	);
 }
