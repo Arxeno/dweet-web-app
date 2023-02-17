5ddac7555337973e8570b6fd8afcc814a248d1ee
raisyam <rmfbkw04@gmail.com>
2023-02-17 16:37:14 +0700

    add login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,27 @@
 const Login = () => {
-	return <h1>Login</h1>
+	return (
+		<div id="login-page">
+			<div id="login-component" className="shadow-effect">
+				<h3>Login</h3>
+
+				<div>
+					<h4>Username</h4>
+					<input />
+				</div>
+
+				<div>
+					<h4>Password</h4>
+					<input type="password"/>
+				</div>
+
+				<button className="button-effect button-yellow">Login</button>
+
+				<hr />
+
+				<p>Need an account? <a href="/signup">Sign up</a></p>
+			</div>
+		</div>
+	)
 }
 
 export default Login
