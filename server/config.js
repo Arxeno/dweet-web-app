d3cd2b08153f624a35b7fa62cd8728ffb6ad5593
raisyam <rmfbkw04@gmail.com>
2023-02-26 20:20:53 +0700

    add environment variable MONGOHOST, MONGOPASSWORD, MONGOPORT, MONGOUSER

--- server/config.js
+++ server/config.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
 
+const MONGO_URL = null;
+
+if (process.env.MONGOHOST && process.env.MONGOPASSWORD && process.env.MONGOPORT && process.env.MONGOUSER) {
+	const {MONGOHOST, MONGOPASSWORD, MONGOPORT, MONGOUSER} = process.env
+
+	MONGO_URL = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`
+}
+
 const CONFIG = {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017',
+  MONGODB_URL: process.env.MONGODB_URL || MONGO_URL || 'mongodb://127.0.0.1:27017',
   MONGODB_DATABASE: process.env.MONGODB_DATABASE || 'DweetDB',
   FRONT_END_URL: process.env.FRONT_END_URL || 'http:/localhost:3000',
 };
